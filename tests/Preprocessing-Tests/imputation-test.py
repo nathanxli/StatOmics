f6ac2e2e9770e47a1e5aaf0b5e7a4ea496a0c59f
nathanxli <nathan.nxli@gmail.com>
2025-04-07 05:48:26 -0400

    Changed imputation test to contain more data

--- tests/Preprocessing-Tests/imputation-test.py
+++ tests/Preprocessing-Tests/imputation-test.py
@@ -2,10 +2,13 @@ import numpy as np
 from rpy2.robjects import DataFrame, FloatVector, StrVector
 from StatOmics.Preprocessing import call
 
+# Simulated expression-like data with missing values
 data = DataFrame({
-    "Gene": StrVector(["g1", "g2"]),
-    "Sample1": FloatVector([1.0, np.nan]),
-    "Sample2": FloatVector([np.nan, 2.0])
+    "ProteinID": StrVector(["P1", "P2", "P3"]),
+    "Sample1": FloatVector([1.2, np.nan, 3.4]),
+    "Sample2": FloatVector([1.5, 2.1, np.nan]),
+    "Sample3": FloatVector([np.nan, 2.2, np.nan]),
+    "Sample4": FloatVector([1.0, 2.3, 3.7])
 })
 
 result = call("imputation_with_normal", data)
