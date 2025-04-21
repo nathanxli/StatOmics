22bff8f21574e317fa3d11d37f67cf991eb568e9
nathanxli <nathan.nxli@gmail.com>
2025-04-21 14:52:52 -0400

    Connected kruskal_rank_sum and run_friedman through python interface

--- StatOmics/Analysis/__init__.py
+++ StatOmics/Analysis/__init__.py
@@ -3,7 +3,16 @@
 from ..r_interface import source_r_script, get_r_function
 
 # Load R script(s) for this module
-source_r_script(["Analysis/mannWhitneyU_test.R", "Analysis/normality_check_byShapi.R", "Analysis/permutation_basedpvalues.R", "Analysis/ttest&BH.R", "Analysis/variance_check_byLeve.R", "Analysis/welcht_test.R"])
+source_r_script([
+    "Analysis/mannWhitneyU_test.R", 
+    "Analysis/normality_check_byShapi.R", 
+    "Analysis/permutation_basedpvalues.R", 
+    "Analysis/ttest&BH.R", 
+    "Analysis/variance_check_byLeve.R", 
+    "Analysis/welcht_test.R",
+    "Analysis/kruskal_rank_sum.R",
+    "Analysis/run_friedman.R"
+])
 
 def call(func_name, *args):
     """
